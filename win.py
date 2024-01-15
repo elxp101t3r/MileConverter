24e2007ac5ffd7e863a08d0fe772e00750eb345c
elxp101t3r <eliotx10101@outlook.com>
2024-01-15 20:17:10 +0200

    Completed!

--- win.py
+++ win.py
@@ -1,8 +1,28 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Entry, Button
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
         self.title("Mile to Km Converter")
         self.minsize(width=400, height=400)
+        self.config(padx=5, pady=5)
+        self.input = Entry(width=10)
+        self.input.grid(padx=70, pady=10,column=1, row=0)
+        self.m_l = Label(text='Miles', font=('Comic', 20, 'italic'))
+        self.m_l.grid(column=2, row=0, padx=70, pady=10)
+        self.eq_l = Label(text='is equal to', font=('Comic', 20, 'italic'))
+        self.eq_l.grid(column=0, row=1)
+        self.num = Label(text=f'{0}', font=('Comic', 20, 'italic'))
+        self.num.grid(column=1, row=1)
+        self.km_l = Label(text='Km', font=('Comic', 20, 'italic'))
+        self.km_l.grid(column=2, row=1)
+        self.calc = Button(text='Calculate', command=self.calculate)
+        self.calc.grid(column=1, row=2)
         self.mainloop()
+    
+    def calculate(self):
+        self.km = 1.6
+        self.num.config(text=f'{float(self.input.get()) * self.km}')
+        
+        
+ 
